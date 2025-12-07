9f28826795ad369ef6e16ec444aa5a1f0ccdf053
Nikola <nikj1705@gmail.com>
2025-12-07 16:18:47 +0200

    Day 7 Part 2 Complete

--- src/main/java/com/example/adventofcode/Day7.java
+++ src/main/java/com/example/adventofcode/Day7.java
@@ -3,9 +3,7 @@ package com.example.adventofcode;
 import lombok.SneakyThrows;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Day7 {
     public final String FILE_DIR = "./src/main/resources/day7.txt";
@@ -14,6 +12,7 @@ public class Day7 {
 
     public Day7() {
         partOne();
+        partTwo();
     }
 
     @SneakyThrows
@@ -39,6 +38,57 @@ public class Day7 {
         }
 
         System.out.println("Day 7 Part 1: " + count);
+    }
+
+    @SneakyThrows
+    private void partTwo() {
+        List<List<Character>> grid = readGrid();
+        Map<String, Long> futures = new LinkedHashMap<>();
+
+        for (int row = 1; row < grid.size(); row++) {
+            for (int col = 0; col < grid.get(row).size(); col++) {
+                String upperCoords = (row - 1) + "_" + col;
+                String currentCoords = row + "_" + col;
+                char upperCell = grid.get(row - 1).get(col);
+                char currentCell = grid.get(row).get(col);
+
+                if (upperCell == 'S') {
+                    grid.get(row).set(col, '|');
+
+                    futures.put(currentCoords, 1L);
+                } else if (currentCell == '^' && upperCell == '|') {
+                    grid.get(row).set(col - 1, '|');
+                    grid.get(row).set(col + 1, '|');
+
+                    String leftCoords = row + "_" + (col - 1);
+                    String rightCoords = row + "_" + (col + 1);
+
+                    long upperFuture = futures.get(upperCoords);
+                    long currentLeftFuture =  futures.getOrDefault(leftCoords, 0L);
+                    long currentRightFuture = futures.getOrDefault(rightCoords, 0L);
+
+                    futures.put(leftCoords, currentLeftFuture + upperFuture);
+                    futures.put(rightCoords, currentRightFuture + upperFuture);
+                } else if (upperCell == '|') {
+                    grid.get(row).set(col, '|');
+
+                    long currentFuture = futures.getOrDefault(currentCoords, 0L);
+                    long upperFuture = futures.getOrDefault(upperCoords, 0L);
+
+                    futures.put(currentCoords, currentFuture + upperFuture);
+                }
+            }
+        }
+
+        String lastRowPrefix = String.valueOf(grid.size() - 1);
+        long sum = 0;
+        for (Map.Entry<String, Long> entry : futures.entrySet()) {
+            if (entry.getKey().startsWith(lastRowPrefix)) {
+                sum += entry.getValue();
+            }
+        }
+
+        System.out.println("Day 7 Part 2: " + sum);
     }
 
     @SneakyThrows
